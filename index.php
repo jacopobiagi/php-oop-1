dc80278d77b554af282b977b1c79adb25056e260
jacopobiagi <jacopo.biagi02@gmail.com>
2024-02-03 17:06:50 +0100

    feat: aggiunta classe 'Categories'

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
+class Categories {
+    public $cat1;
+    public $cat2;
+
+    function __construct($_cat1, $_cat2){
+        $this -> cat1 = $_cat1;
+        $this -> cat2 = $_cat2;
+    }
+}
+
 class Movie {
     public $name;
     public $categories;
     public $publishDate;
 
-    function __construct($_name, $_categories, $_publishDate){
+    function __construct($_name, Categories $_categories, $_publishDate){
         $this -> name = $_name;
         $this -> categories = $_categories;
         $this -> publishDate = $_publishDate;
@@ -16,14 +26,17 @@ class Movie {
     }
 }
 
-$movie1 = new Movie ('nemo', 'animazione', '2003');
+$categories1 = new Categories ('animazione', 'fantasy');
+$categories2 = new Categories ('animazione', 'avventura');
+
+$movie1 = new Movie ('nemo', $categories1, '2003');
 var_dump($movie1);
 echo '<br>';
 echo 'nome:'.$movie1 -> getName().'';
 
 echo '<br>------------------------------------<br>';
 
-$movie2 = new Movie ('UP', 'animazione', '2009');
+$movie2 = new Movie ('UP', $categories2, '2009');
 var_dump($movie2);
 echo '<br>';
 echo 'nome:'.$movie2 -> getName().'';
